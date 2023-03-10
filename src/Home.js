06370a4c374fded146853b71320a3d466693df2e
Ryan Robinson <Ryan.Robinson@ao.com>
2023-03-10 17:18:31 +0000

    refactor, use prop and destructuring

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 const [blogs, setBlogs] = useState([
@@ -8,14 +9,8 @@ const [blogs, setBlogs] = useState([
 ])  
 
   return (
-    <div className="home">
-      {blogs.map((blog) => (
-       <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-       </div>
-      ))}
-
+    <div className="home"> 
+      <BlogList blogs={blogs} title="All Blogs!"/>
     </div>
   );
 };

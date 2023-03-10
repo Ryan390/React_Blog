a4cbc10e9ecc3dc0d3779955a54bcbde059e5850
Ryan Robinson <Ryan.Robinson@ao.com>
2023-03-10 17:24:45 +0000

    start wiring up the delete blog button

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,15 @@ const [blogs, setBlogs] = useState([
   {title: 'Hello website!', body: 'lorem ipsum...', author: 'mario', id:1},
   {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id:2},
   {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id:3},
-])  
+])
+
+const handleDelete = (id) => {
+  console.log(id);
+}
 
   return (
     <div className="home"> 
-      <BlogList blogs={blogs} title="All Blogs!"/>
+      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
     </div>
   );
 };
